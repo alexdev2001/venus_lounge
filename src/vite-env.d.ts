35a41c597774903b0fdc951e89410c2a089051c8
alexdev2001 <aleximani201@gmail.com>
2025-08-28 10:42:42 +0200

    modified tsconfig file

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.png";
+declare module "*.svg";
+declare module "*.gif";
+declare module "*.webp";
+declare module "*.css";
