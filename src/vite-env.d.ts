598b66a9a9812e88239f3803f7fe6c6834671dc6
alexdev2001 <aleximani201@gmail.com>
2025-08-28 09:43:47 +0200

    final fix

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+// For images and other assets
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.png";
+declare module "*.svg";
+declare module "*.gif";
+declare module "*.webp";
+
+// For CSS modules
+declare module "*.css";
